c2358d8b8a3b9443cda92e1ce781c6fa9bdd9deb
zachjd <zachjd@users.noreply.github.com>
2020-09-18 17:36:03 -0600

    Update PrinterAssist.ps1

--- PrinterAssist.ps1
+++ PrinterAssist.ps1
@@ -1,50 +1,40 @@
-$formWSD3PrinterAssist_Load = {
-	#Find only copiers for combobox selection
-	Function Get-Copier
-	{
-		Get-Printer | foreach { $_.Name } |
-		Where-Object {
-			$_ -Match "CPY" }
+# Find only copiers in combobox selection
+Function Get-Copier
+{
+	Get-Printer | foreach { $_.Name } |
+	Where-Object {
+		$_ -Match "CPY"
 	}
-		$copiers = Get-Copier
-		
-		ForEach ($name in $copiers)
-		{
-			$comboboxCopier.Items.Add($name)
-		}
-	
-	#Find all printers to run a test page
-	Function Get-TestPageList
-	{
-		Get-Printer | foreach { $_.Name } |
-		Where-Object {
-			$_ -Match "CPY" -or $_ -Match "PRN"
-		}
-	}
-	$tplist = get-testpagelist
-		
-		ForEach ($tp in $tplist)
-		{
-			$comboboxTestPrint.Items.Add($tp)
-		}
+}
+$copiers = Get-Copier
+ForEach ($name in $copiers)
+{
+	$comboboxCopier.Items.Add($name)
 }
 
-#Runs Group Policy Update
+# Find printers and copiers in combobox for test print
+Function Get-TestPageList
+{
+	Get-Printer | foreach { $_.Name } |
+	Where-Object {
+		$_ -Match "CPY" -or $_ -Match "PRN"
+	}
+}
+$tplist = get-testpagelist
+ForEach ($tp in $tplist)
+{
+	$comboboxTestPrint.Items.Add($tp)
+}
+
+# Runs a group policy update
 $btnUpdate_Click = {
-	$btnUpdate.Enabled = $false
-	
-	$btnUpdate.text = "Please wait.."
-		
-	$printerOutputbox.lines = "Updating group policy..."
-	
-	gpupdate /force
-	
-	$printerOutputbox.lines = ""
-	
-	$this.Enabled = $true
-	
-	$btnUpdate.text = "Update"
-	
+	$btnUpdate.Enabled = $false	
+	$btnUpdate.text = "Please wait.."		
+	$printerOutputbox.lines = "Updating group policy..."	
+	gpupdate /force	
+	$printerOutputbox.lines = ""	
+	$this.Enabled = $true	
+	$btnUpdate.text = "Update"	
 	$a = new-object -comobject wscript.shell	
 	$intAnswer = $a.popup("A restart is needed for completion, do you want to continue?", `
 		0, "Restart Computer", 4)
@@ -58,72 +48,68 @@ $btnUpdate_Click = {
 	}
 }
 
-#Search Printers
+# Search and list all printers while only looking for CPY and PRN
 $buttonMyPrinters_Click = {
 	$buttonMyPrinters.Enabled = $false
-	
-	$buttonMyPrinters.Text = "Loading.."
-	
+	$buttonMyPrinters.Text = "Loading.."	
 	function get-printers
 	{
 		Get-Printer | Select-Object Name, Location, PrinterStatus |
 		Where-Object {
-			$_ -NotMatch "Microsoft Print to PDF" -and $_ -NotMatch "Fax" -and
-			$_ -NotMatch "Adobe PDF" -and $_ -NotMatch "Microsoft XPS Document Writer" -and
-			$_ -NotMatch "Send to OneNote 16" -and $_ -NotMatch "OneNote for Windows 10"
+			$_ -Match "CPY" -or $_ -Match "PRN" -or $_ -Match "PLT"
 		} |
 		ForEach-Object {
 			"Printer Name: $($_.Name -replace "(wsd-prn01)", $blank -replace "(\\)", $blank -replace "(whs-fsps)",
 				$blank -replace "(sjh-fsps)", $blank -replace "(mhs-fsps)", $blank -replace "(jjh-fsps)", $blank -replace "(whs12a)",
 				$blank -replace "(wjh-fsps)", $blank)" + "`r`n" + "Location: $($_.Location)" + "`r`n" + "Status: $($_.PrinterStatus)" + "`r`n"
-		}
+		}		
 	}
 	
 	$printerOutputbox.lines = get-printers
 	
-	$buttonMyPrinters.Enabled = $true
+#Broken at the moment but does not affect script. It alerts you that no printers are found.
+	if ($printeroutbox.text = "")
+	{
+		[System.Windows.Forms.MessageBox]::Show("No printers found.", "Information", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Asterisk)
+	}
+	else {}
 	
+	$buttonMyPrinters.Enabled = $true	
 	$buttonMyPrinters.Text = "My Printers"
-}
+	}
 
-#Enable print code button upon selection
+# Enable print code button upon combobox selection
 $comboboxCopier_SelectedIndexChanged = {
 	$printcodeButton.Enabled = $true
-}
+	}
 
-#Enable test print button upon selection
+# Enable test print button upon selection
 $comboboxTestPrint_SelectedIndexChanged = {
 	$testprintButton.Enabled = $true
-}
+	}
 
-#Open preferences
+# Open copier preferences from the selected copier
 $printcodeButton_Click = {
-	$copiername = $comboboxCopier.SelectedItem
-	
-	[System.Windows.Forms.MessageBox]::Show("To enter your printer code, click the 'JOB HANDLING' tab then enter your 'USER NUMBER' and click 'APPLY'", "Information", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Asterisk)
-	
+	$copiername = $comboboxCopier.SelectedItem	
+	[System.Windows.Forms.MessageBox]::Show("To enter your printer code, click the 'JOB HANDLING' tab then enter your 'USER NUMBER' and click 'APPLY'. For some copiers, you'll need to click 'AUTHENTICATION', then select 'USER NUMBER'.", "Information", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Asterisk)
 	cmd.exe /c RUNDLL32 PRINTUI.DLL, PrintUIEntry /e /n $copiername
-}
+	}
 
-#Double click to copy text
+# Double click to copy text
 $printerOutputbox_DoubleClick = {
-	$printerOutputbox.SelectAll()
-	
-	Set-Clipboard -Value $printerOutputbox.text
-	
+	$printerOutputbox.SelectAll()	
+	Set-Clipboard -Value $printerOutputbox.text	
 	[System.Windows.Forms.MessageBox]::Show("Copied to clipboard.", "Copied", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Asterisk)
-	
 	$printerOutputbox.DeselectAll()
-}
+	}
 
-#Ability to right click
+# Ability to right click
 $copyToolStripMenuItem_Click = {
 	Set-Clipboard -Value $printerOutputbox.text
-}
+	}
 
-#Prints a test page for selected printer
+# Runs the CMD command for a test print pointed to the chosen printer 
 $testprintButton_Click = {
 	$testprints = $comboboxTestPrint.SelectedItem
-	cmd.exe /c RUNDLL32 PRINTUI.DLL, PrintUIEntry /e /n $testprints /k
-	
-}
+	cmd.exe /c RUNDLL32 PRINTUI.DLL, PrintUIEntry /e /n $testprints /k	
+	}
